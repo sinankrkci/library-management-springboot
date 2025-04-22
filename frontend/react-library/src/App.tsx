934a7c76baf013d5b91d9383bbc683a66eb38d28
skara <sinankarakeci@gmail.com>
2025-04-22 23:14:59 +0300

    Frontend: add react routing

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -1,19 +1,28 @@
-
-import './App.css';
-import { Navbar } from './layouts/NavbarAndFooter/Navbar';
-import { Footer } from './layouts/NavbarAndFooter/Footer';
-import { HomePage } from './layouts/HomePage/HomePage';
-import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
+import "./App.css";
+import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
+import { Footer } from "./layouts/NavbarAndFooter/Footer";
+import { HomePage } from "./layouts/HomePage/HomePage";
+import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
+import { Redirect, Route, Switch } from "react-router-dom";
 
 export const App = () => {
   return (
-    <div>
-        <Navbar />
-        {/* <HomePage /> */}
-        <SearchBooksPage />
-        <Footer />
+    <div className="d-flex flex-column min-vh-100">
+      <Navbar />
+      <div className="flex-grow-1">
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/home" />
+          </Route>
+          <Route path="/home">
+            <HomePage />
+          </Route>
+          <Route path="/search">
+            <SearchBooksPage />
+          </Route>
+        </Switch>
+      </div>
+      <Footer />
     </div>
   );
-}
-
-
+};
