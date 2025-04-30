fa9f3029d43ef88d22f1e36163820f4c94af84ca
skara <sinankarakeci@gmail.com>
2025-04-30 20:01:17 +0300

    Refactor Home page

--- frontend/react-library/src/layouts/HomePage/components/Heros.tsx
+++ frontend/react-library/src/layouts/HomePage/components/Heros.tsx
@@ -1,4 +1,9 @@
+import { useOktaAuth } from "@okta/okta-react";
+import { Link } from "react-router-dom";
+
 export const Heros = () => {
+  const { authState } = useOktaAuth();
+
   return (
     <div>
       <div className="d-none d-lg-block">
@@ -15,9 +20,19 @@ export const Heros = () => {
                 Whether it is to learn a new skill or grow within one, we will
                 be able to provide the content for you.
               </p>
-              <a className="btn main-color btn-lg text-white" href="#">
-                Sign up
-              </a>
+              {authState?.isAuthenticated ? (
+                <Link
+                  type="button"
+                  className="btn main-color btn-lg text-white"
+                  to="search"
+                >
+                  Explore top books
+                </Link>
+              ) : (
+                <Link className="btn main-color btn-lg text-white" to="/login">
+                  Sign up
+                </Link>
+              )}
             </div>
           </div>
         </div>
@@ -32,8 +47,8 @@ export const Heros = () => {
                 Try to check in daily as your colleciton is always changing! We
                 work nonstop to provide the most accurate book selection
                 possible for oue Luv 2 Read students! We are diligent about our
-                book selection and our books are always
-                going to be our top priority.
+                book selection and our books are always going to be our top
+                priority.
               </p>
             </div>
           </div>
@@ -55,9 +70,19 @@ export const Heros = () => {
                 Whether it is to learn a new skill or grow within one, we will
                 be able to provide the content for you.
               </p>
-              <a className="btn main-color btn-lg text-white" href="#">
-                Sign up
-              </a>
+              {authState?.isAuthenticated ? (
+                <Link
+                  type="button"
+                  className="btn main-color btn-lg text-white"
+                  to="search"
+                >
+                  Explore top books
+                </Link>
+              ) : (
+                <Link className="btn main-color btn-lg text-white" to="login">
+                  Sign up
+                </Link>
+              )}
             </div>
           </div>
           <div className="m-2">
@@ -68,8 +93,8 @@ export const Heros = () => {
                 Try to check in daily as your colleciton is always changing! We
                 work nonstop to provide the most accurate book selection
                 possible for oue Luv 2 Read students! We are diligent about our
-                book selection and our books are always
-                going to be our top priority.
+                book selection and our books are always going to be our top
+                priority.
               </p>
             </div>
           </div>
